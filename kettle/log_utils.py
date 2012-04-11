ab54cef5ea186f33a4e08844ad281c5591b195cf
Christy O'Reilly <christy@oreills.co.uk>
2012-04-11 16:36:04 +0100

    Fix logging more

--- kettle/log_utils.py
+++ kettle/log_utils.py
@@ -9,7 +9,9 @@ def get_thread_handlers():
     # Return a list of log handlers handling this thread
     # They are reversed so that NestedSetup(get_thread_handlers()) gives an
     # identical handler to the ones already in place
-    return list(reversed(Handler.stack_manager.iter_context_objects()))
+    handlers = list(Handler.stack_manager.iter_context_objects())
+    handlers.reverse()
+    return handlers
 
 def inner_thread_nested_setup(outer_handlers):
     return NestedSetup([h for h in outer_handlers if h not in get_thread_handlers()])
