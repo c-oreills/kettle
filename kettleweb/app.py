2a9c5a8f6ac0501893bbaaf79f1ea54402a174ec
William Cooke <will@conversocial.com>
2016-05-17 17:12:44 +0100

    fix: base64 image needs to be set as a config param

--- kettleweb/app.py
+++ kettleweb/app.py
@@ -22,6 +22,7 @@ app.secret_key = settings.SECRET_KEY
 app.config['ROLLOUT_REFRESH_TIMEOUT'] = getattr(settings, 'ROLLOUT_REFRESH_TIMEOUT', 1000)
 app.config['CHECKLIST_URL'] = getattr(settings, 'CHECKLIST_URL', None)
 app.config['CHECKLIST_HEIGHT'] = getattr(settings, 'CHECKLIST_HEIGHT', None)
+app.config['CHECKLIST_CLICKTHROUGH_IMAGE_BASE64'] = getattr(settings, 'CHECKLIST_CLICKTHROUGH_IMAGE_BASE64', None)
 rollout_cls = settings.get_cls(settings.ROLLOUT_CLS)
 rollout_form_cls = settings.get_cls(settings.ROLLOUT_FORM_CLS)
 
