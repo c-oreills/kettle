7d5d3775d343c99cab6a309221820aba9ff8e953
William Cooke <will@conversocial.com>
2016-05-17 17:26:47 +0100

    fix: img height shouldnt be 100% and should replace the child

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os
 from setuptools import setup, find_packages
 
-version='0.5.3'
+version='0.5.4'
 
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
