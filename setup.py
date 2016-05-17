b20dd405914a0408fbf16db982a8b94de7cbc23a
William Cooke <will@conversocial.com>
2016-05-17 17:20:48 +0100

    fix: $.(...).replace isn't a function

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os
 from setuptools import setup, find_packages
 
-version='0.5.2'
+version='0.5.3'
 
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
